993894b2b4585dbdf25228ecacdfb1b1afbb63ea
Nicolai Brand <nicolaihbrand@gmail.com>
2023-01-21 15:47:55 +0100

    hashtable: memory management is handled by the caller & improved interface (#17)

--- examples/ht_test.c
+++ examples/ht_test.c
@@ -1,56 +1,41 @@
 /*
- * Written by Nicolai H. Brand (lytix.dev) 2022.
+ * Written by Nicolai H. Brand (lytix.dev) 2022-2023.
  * Example usage of the hashtable implementation found in nicc.
  * Licensed under GPLv3, see the LICENSE file for more info.
  */
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #define NICC_HT_IMPLEMENTATION
 #include "../nicc.h"
 
 
-/* wrapper functions when key and val are string */
-void ht_sset(struct ht_t *table, const char *key, const char *val)
-{
-    ht_set(table, key, strlen(key) + 1, val, strlen(val) + 1, NULL);
-}
-
-char *ht_sget(struct ht_t *table, const char *key)
-{
-    return ht_get(table, key, strlen(key) + 1);
-}
-
 int main(void)
 {
-    struct ht_t *table = ht_malloc(32);
+    struct ht_t *table = ht_alloc(32);
 
     char *key1 = "my key"; 
     char *val1 = "hello world!";
     ht_sset(table, key1, val1);
+    assert(ht_sget(table, key1) == val1);
 
     /* example where value is a a struct */
     typedef struct {
         int a;
         char str[32];
     } my_struct;
+
     char *key2 = "my second key";
     my_struct mst = { .a = 10, .str = "hello" };
-    ht_set(table, key2, strlen(key2) + 1, &mst, sizeof(mst), NULL);
+    ht_set(table, key2, strlen(key2) + 1, &mst);
+    assert(memcmp((my_struct *)ht_sget(table, key2), &mst, sizeof(my_struct)) == 0);
 
-    /* results */
-    char *res1 = ht_sget(table, key1);
-    my_struct *res2 = ht_get(table, key2, strlen(key2) + 1);
-    printf("%s = %s\n", val1, res1);
-    printf("%s = %s\n", mst.str, res2->str);
+    ht_srm(table, key1);
 
-    struct ht_item_t **raw = ht_raw(table);
-
-    for (size_t i = 0; raw[i] != NULL; i++)
-        printf("%s = %x\n", (char *)raw[i]->key, *(int *)raw[i]->value);
-
-    free(raw);
+    void **res = ht_raw(table);
+    assert(memcmp(res[0], &mst, sizeof(my_struct)) == 0);
     ht_free(table);
-    
+
     return 0;
 }
