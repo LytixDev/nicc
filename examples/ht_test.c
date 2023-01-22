ad51f455f49d33df23b47072fbcb1a5c3d561041
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-01-22 14:29:52 +0100

    store last byte of hash and alloc flag

--- examples/ht_test.c
+++ examples/ht_test.c
@@ -11,6 +11,13 @@
 #include "../nicc.h"
 
 
+void foo(struct ht_t *table)
+{
+    char *str = "this string is copied to the heap!";
+    /* ht_set_alloc, ht_sset_alloc and ht_sset_alloc_T will copy the data over to the heap */
+    ht_sset_alloc(table, "heap", str, strlen(str) + 1);
+}
+
 int main(void)
 {
     struct ht_t *table = ht_alloc(32);
@@ -31,10 +38,18 @@ int main(void)
     ht_set(table, key2, strlen(key2) + 1, &mst);
     assert(memcmp((my_struct *)ht_sget(table, key2), &mst, sizeof(my_struct)) == 0);
 
+    assert(ht_get_len(table) == 2);
     ht_srm(table, key1);
+    assert(ht_get_len(table) == 1);
 
     void **res = ht_raw(table);
     assert(memcmp(res[0], &mst, sizeof(my_struct)) == 0);
+
+    foo(table);
+    char *str = ht_sget(table, "heap");
+    assert(strcmp(str, "this string is copied to the heap!") == 0);
+    ht_srm(table, "heap");
+
     ht_free(table);
 
     return 0;
